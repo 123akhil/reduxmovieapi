2b78cbaf553a834f979cb94a95bd83e050027a8e
Your Name <chaudharyakhil337@gmail.com>
2022-02-19 15:50:09 +0530

    commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,25 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/movieApi";
-import { APIKey } from "../../common/apis/MovieApiKey";
+// import movieApi from "../../common/apis/movieApi";
+// import { APIKey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../redux/movies/movieSlice";
+import { fetchAsyncMovies } from "../../redux/movies/movieSlice";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const movieText = "Harry";
-  // api call
+  // const movieText = "Harry";
+  // api call using sychronus action creator but we will be using thunk middleware to make it async action creator
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log("Err :", err);
-        });
-      dispatch(addMovies(response.data));
-    };
-
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+    // const fetchMovies = async () => {
+    // const response = await movieApi
+    //   .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+    //   .catch((err) => {
+    //     console.log("Err :", err);
+    //   });
+    // dispatch(addMovies(response.data));
+    // };
+  }, [dispatch]);
 
   return (
     <div>
